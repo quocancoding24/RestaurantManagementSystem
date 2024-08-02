8532b9b7939864eff911bd9d4555ba42a46585aa
Quoc An <an.tranquoc.cit22@eiu.edu.vn>
2024-08-02 15:11:16 +0700

    MenuItem

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,5 +1,23 @@
 public class MenuItem {
          private String name;
          private double price;
-sdkfjkdfjsdklfjjskfjdkfdslfdsfsdfsdfsdfsdfsd
+         public MenuItem() {
+         }
+         public MenuItem(String name, double price) {
+                  this.name = name;
+                  this.price = price;
+         }
+         public String getName() {
+                  return name;
+         }
+         public void setName(String name) {
+                  this.name = name;
+         }
+         public double getPrice() {
+                  return price;
+         }
+         public void setPrice(double price) {
+                  this.price = price;
+         }
+         
 }
